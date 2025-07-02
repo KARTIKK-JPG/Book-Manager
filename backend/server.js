226d64474d532c894eed52b151e8f08018134e13
Kartik Fulara <kartikfulara786@gmail.com>
2025-07-03 00:25:24 +0530

    corse error

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://book-manager-5z9q.vercel.app"
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: "*",
+  credentials: true
 }));
 
 
