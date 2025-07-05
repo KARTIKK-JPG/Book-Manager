883b313e94a6f8d70c069595a8f16e278ededaf2
KARTIKK-JPG <kartikfulara786@gmail.com>
2025-07-05 18:24:07 +0530

    Update (cors origin) server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://book-manager-frontend-mu.vercel.app"
+    'https://book-manager-frontend-c2kdtyc9p-kartiks-projects-a2175768.vercel.app'
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
